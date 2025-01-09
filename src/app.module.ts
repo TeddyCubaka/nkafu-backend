c73db4d61fb8abeac75ed09e73bb980092ee3582
Teddy Cubaka <teddycubaka96@gmail.com>
2025-01-09 16:20:43 +0100

    delete the ressource operation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { CoreModule } from './core/core.module';
-import { OperationModule } from './operation/operation.module';
 
 @Module({
   imports: [
@@ -14,7 +13,6 @@ import { OperationModule } from './operation/operation.module';
     }),
     AuthModule,
     CoreModule,
-    OperationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/operation/operation.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { OperationService } from './operation.service';
-
-@Controller('operation')
-export class OperationController {
-  constructor(private readonly operationService: OperationService) {}
-}
