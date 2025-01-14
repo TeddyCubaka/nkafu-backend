14a5a403450871bc2e23034804b7acde550de6df
Teddy Cubaka <teddycubaka96@gmail.com>
2025-01-14 15:24:49 +0100

    add an endpoint to return all agent infos

--- src/mobile/mobile.controller.ts
+++ src/mobile/mobile.controller.ts
@@ -1,34 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards, Res } from '@nestjs/common';
 import { MobileService } from './mobile.service';
-import { CreateMobileDto } from './dto/create-mobile.dto';
-import { UpdateMobileDto } from './dto/update-mobile.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('mobile')
 export class MobileController {
   constructor(private readonly mobileService: MobileService) {}
 
-  @Post()
-  create(@Body() createMobileDto: CreateMobileDto) {
-    return this.mobileService.create(createMobileDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.mobileService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.mobileService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMobileDto: UpdateMobileDto) {
-    return this.mobileService.update(+id, updateMobileDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.mobileService.remove(+id);
+  @Get('agent-info/:uuid')
+  @UseGuards(JwtAuthGuard)
+  async findAll(@Param('uuid') uuid, @Res() res) {
+    const data = await this.mobileService.getAgentInfo(uuid);
+    return res.status(data.code).json(data);
   }
 }
--- src/mobile/mobile.service.ts
+++ src/mobile/mobile.service.ts
@@ -1,26 +1,139 @@
 import { Injectable } from '@nestjs/common';
-import { CreateMobileDto } from './dto/create-mobile.dto';
-import { UpdateMobileDto } from './dto/update-mobile.dto';
+import { prisma } from 'prisma/lib/prisma';
 
 @Injectable()
 export class MobileService {
-  create(createMobileDto: CreateMobileDto) {
-    return 'This action adds a new mobile';
-  }
+  async getAgentInfo(id: string) {
+    const agent = await prisma.agent.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        firstName: true,
+        middleName: true,
+        lastName: true,
+        mobile: true,
+        mail: true,
+        address: true,
+        createdAt: true,
+        organization: {
+          select: {
+            id: true,
+            name: true,
+            photo: true,
+          },
+        },
+        operationClosed: {
+          take: 2,
+          orderBy: { createdAt: { sort: 'desc' } },
+          select: {
+            id: true,
+            recipe: {
+              select: {
+                id: true,
+                name: true,
+                pricing: true,
+                currency: {
+                  select: {
+                    id: true,
+                    name: true,
+                    symbol: true,
+                    formatKey: true,
+                    exchangeRate: true,
+                  },
+                },
+                description: true,
+                recipeType: true,
+                generatingFact: true,
+                activitySector: {
+                  select: {
+                    id: true,
+                    name: true,
+                  },
+                },
+              },
+            },
+            possession: {
+              select: {
+                id: true,
+                uniqueNumber: true,
+                type: true,
+                taxPayer: true,
+                createdAt: true,
+              },
+            },
+            transactions: {
+              include: {
+                wallet: { include: { currency: true } },
+              },
+            },
+          },
+        },
+        operationInitializated: {
+          take: 2,
+          orderBy: { createdAt: { sort: 'desc' } },
+          select: {
+            id: true,
+            recipe: {
+              select: {
+                id: true,
+                name: true,
+                pricing: true,
+                currency: {
+                  select: {
+                    id: true,
+                    name: true,
+                    symbol: true,
+                    formatKey: true,
+                    exchangeRate: true,
+                  },
+                },
+                description: true,
+                recipeType: true,
+                generatingFact: true,
+                activitySector: {
+                  select: {
+                    id: true,
+                    name: true,
+                  },
+                },
+              },
+            },
+            possession: {
+              select: {
+                id: true,
+                uniqueNumber: true,
+                type: true,
+                taxPayer: true,
+                createdAt: true,
+              },
+            },
+            transactions: {
+              include: {
+                wallet: { include: { currency: true } },
+              },
+            },
+          },
+        },
+        liquidations: {
+          take: 20,
+          orderBy: { createdAt: { sort: 'desc' } },
+        },
+        entity: true,
+        wallets: {
+          include: { currency: true },
+        },
+      },
+    });
 
-  findAll() {
-    return `This action returns all mobile`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} mobile`;
-  }
-
-  update(id: number, updateMobileDto: UpdateMobileDto) {
-    return `This action updates a #${id} mobile`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} mobile`;
+    if (agent == null)
+      return {
+        code: 404,
+        message: 'aucun agent.e trouvé.e',
+      };
+    return {
+      code: 200,
+      message: ' voici les informations sur cet agent',
+      data: agent,
+    };
   }
 }
