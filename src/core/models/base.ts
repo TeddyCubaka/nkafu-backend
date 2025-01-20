8e0a2c1a133d2a04405652e80b2fd8243c64f128
Teddy Cubaka <teddycubaka96@gmail.com>
2025-01-20 13:46:29 +0100

    add id in all selected nested element

--- src/core/models/base.ts
+++ src/core/models/base.ts
@@ -113,15 +113,22 @@ export abstract class BaseModel<T extends keyof PrismaClient> {
   }
 
   generateInclude(): Record<string, any> {
-    const include: Record<string, any> = { id: true };
+    const select: Record<string, any> = { id: true };
     if (this.listColumns == '*') return {};
     this.listColumns.forEach((column) => {
       const keys = column.proprety.split('.');
-      let currentLevel = include;
+      let currentLevel = select;
 
       keys.forEach((key, index) => {
         if (!currentLevel[key]) {
-          currentLevel[key] = index === keys.length - 1 ? true : { select: {} };
+          currentLevel[key] =
+            index === keys.length - 1
+              ? true
+              : {
+                  select: {
+                    id: true,
+                  },
+                };
         }
         if (index < keys.length - 1) {
           currentLevel = currentLevel[key].select;
@@ -129,6 +136,6 @@ export abstract class BaseModel<T extends keyof PrismaClient> {
       });
     });
 
-    return include;
+    return select;
   }
 }
